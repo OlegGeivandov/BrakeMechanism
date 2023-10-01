0c9f4059e6b688b3f60944200d5adc157be82f90
OlegGeivandov <140657755+OlegGeivandov@users.noreply.github.com>
2023-10-01 21:57:32 +0300

    1

--- src/BrakeMechanism.java
+++ src/BrakeMechanism.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class BrakeMechanism {
+import java.util.ArrayList;
+
+public class BrakeMechanism {
+    ArrayList<Integer> firstBrakePad;
+    ArrayList<Integer> secondBrakePad;
+    int minimumLegalBrakeLength;
+    int min, tempMin;
+
+    public BrakeMechanism(ArrayList<Integer> firstBrakePad, ArrayList<Integer> secondBrakePad) {
+        if (firstBrakePad.size() >= secondBrakePad.size()) {
+            this.firstBrakePad = firstBrakePad;
+            this.secondBrakePad = secondBrakePad;
+        }
+        else {
+            this.firstBrakePad = secondBrakePad;
+            this.secondBrakePad = firstBrakePad;
+        }
+        this.minimumLegalBrakeLength = firstBrakePad.size() + secondBrakePad.size();
+        this.min = minimumLegalBrakeLength;
+        this.tempMin = minimumLegalBrakeLength;
+    }
+
+    void calculateLegalBrakeLength(){
+        firstPartCalculate();
+        secondPartCalculate();
+    }
+
+    private void firstPartCalculate() {
+        for (int i = 0; i < firstBrakePad.size(); i++) {
+            boolean isGoodProfile = true;
+            for (int j = secondBrakePad.size()-1, k=i; j>=0 && k>=0 ; j--, k--) {
+                if (firstBrakePad.get(k) + secondBrakePad.get(j) > 3) isGoodProfile = false;
+            }
+            if (isGoodProfile && i < secondBrakePad.size()) tempMin = firstBrakePad.size() + secondBrakePad.size() - 1 - i;
+            if (isGoodProfile && i >= secondBrakePad.size()) tempMin = firstBrakePad.size();
+            if (tempMin < min) min = tempMin;
+        }
+        if (min < minimumLegalBrakeLength) minimumLegalBrakeLength = min;
+    }
+
+    private void secondPartCalculate() {
+        for (int i = firstBrakePad.size()-secondBrakePad.size()+1; i < firstBrakePad.size(); i++) {
+            boolean isGoodProfile = true;
+            for (int j = 0, k=i; j<firstBrakePad.size()-i; j++, k++) {
+                if (firstBrakePad.get(k) + secondBrakePad.get(j) > 3) isGoodProfile = false;
+            }
+            if (isGoodProfile) tempMin = secondBrakePad.size() + i;
+            if (tempMin < min) min = tempMin;
+        }
+        if (min < minimumLegalBrakeLength) minimumLegalBrakeLength = min;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Main {
+
+public static BrakeMechanism bm;
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String fileName = "1.txt";
+        readFromFile(fileName);
+        bm.calculateLegalBrakeLength();
+        System.out.println(bm.minimumLegalBrakeLength);
+    }
+
+
+    static void readFromFile (String fileName) {
+        try {
+            Scanner scanner = new Scanner(new File(fileName));
+            bm = new BrakeMechanism(getArrayListFromString(scanner.nextLine()), getArrayListFromString(scanner.nextLine()));
+            scanner.close();
+            System.out.println(bm.firstBrakePad);
+            System.out.println(bm.secondBrakePad);
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("упс, файл "+fileName+" не найден");
+        }
+        catch (InputMismatchException e){
+            System.out.println("в файле не совсем число записано");
+        }
+    }
+
+
+    public static ArrayList<Integer> getArrayListFromString(String str) {
+        ArrayList<Integer> list = new ArrayList<>();
+        char[] charArray = str.toCharArray();
+        for (Character ch : charArray) {
+            list.add(Integer.parseInt(String.valueOf(ch)));
+        }
+        return list;
     }
 }
